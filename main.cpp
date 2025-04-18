3520cc8053f3d7894bb36c4e099731da0154a12f
WangWindow <1598593280@qq.com>
2025-04-18 15:27:11 +0800

    [update] with cmake 4.0.1 + msvc

--- main.cpp
+++ main.cpp
@@ -2,4 +2,10 @@ import std;
 
 auto main() -> int {
     std::println("Hello, World!");
+
+    std::vector<int> numbers = { 1, 2, 3, 4, 5 };
+
+    std::for_each(numbers.begin(), numbers.end(), [](int n) {
+        std::println("Number: {}", n);
+    });
 }
